cab673befa8ff4104dd378c04e789b2ea6ff3319
Anthony Chin <anthony.a.chin@ericsson.com>
2013-12-06 17:47:19 -0500

    Finished GrafixMask

--- src/main/java/codingAlgo/grafixMask.java
+++ src/main/java/codingAlgo/grafixMask.java
@@ -1,92 +1,120 @@
 package codingAlgo;
 
-public class grafixMask {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Stack;
+
+/**
+* Single Round Match 211 Round 1 - Division I, Level Two
+* http://www.topcoder.com/stat?c=problem_statement&pm=2998&rd=5857
+* 
+* @author Anthony Chin
+*/
+public class GrafixMask {
 	
 	public static final int fillX = 600;
 	public static final int fillY = 400;
 
 	public static boolean[][] grid;
 
-	public static void main(String[] args){
-//		String s = "5 4 3 2";
-//		System.out.println(s);
-//		String[] splittedString = s.split("\\s");
-//		for (String string : splittedString) {
-//			System.out.println(string);
-//		}
-		//String[] test =  {"4 3 2 5", "2 2 2 2", "300 399 500 199"};
-		String[] test = {"0 292 399 307"};
-		sortedArea(test);
+	public int[] sortedArea (String[] rectangles){
+		grid = new boolean[600][400];
+		cleanRectangle();
+		maskRectangle(rectangles);
+
+		//Find the area for each hole: in pixel
+		ArrayList<Integer> result = new ArrayList<Integer>();
+		for(int x = 0; x < grid.length; x++){
+			for(int y = 0; y < grid[x].length; y++){
+				if(grid[x][y]) {
+					result.add(doFill(x,y));
+					int pixel = doFill(x,y);
+					if (pixel != 0) {
+						result.add(doFill(x,y));
+					}
+				}
+			}
+		}
+
+		int[] arrayResult = convertIntegersListToArray(result);
+		Arrays.sort(arrayResult);
+		return arrayResult;
+		
 	}
 
-/*	if (!grid[x][y]){
-		//System.out.println(++check);
-		System.out.println(grid[x][y] + "x is: " + x + "y is: " + y);
-		result[0] += doFill(fillX, fillY);
-		result[1] += doFill(fillX, fillY);+
-	}*/
-	
-	public static int[] sortedArea (String[] rectangles){
-		
-		grid = new boolean[600][400];
+	public int doFill(int x, int y) {
+		int result = 0;
+		Stack<Node> stack = new Stack<Node>();
+		stack.push(new Node(x,y));
 
+		while (!stack.isEmpty()) {
+			Node top = stack.peek();
+			stack.pop();
+
+			if (top.x < 0 || top.x >= fillX) { continue; }
+			if (top.y < 0 || top.y >= fillY) { continue; }
+			if (!grid[top.x][top.y]) { continue; }
+
+			grid[top.x][top.y] = false;
+			result++;
+			stack.push(new Node(top.x + 1, top.y));
+			stack.push(new Node(top.x - 1, top.y));
+			stack.push(new Node(top.x, top.y + 1));
+			stack.push(new Node(top.x, top.y - 1));
+		}
+		return result;
+	}
+
+	private void cleanRectangle() {
 		// fill the masking layer to true
 		for(int x = 0; x < grid.length; x++){
 			for(int y = 0; y < grid[x].length; y++){
 				grid[x][y] = true;
 			}
 		}
+	}
 
+	private void maskRectangle (String[] rectangles) {
 		for(String rectangle : rectangles) {
 			//split the string to remove all white space
 			// '\s' removes a whitespace character [ \t\n\x0B\f\r]
 			// beginning with '\' is an escape construct. (need to use twice for it to compile)
 			
-			// Takke the string { 0, 292, 399, 307 } and break it into individual integer.
+			// Take the string { 0, 292, 399, 307 } and break it into individual integer.
 			String[] rectangleSplit = rectangle.split("\\s");
 			int[] rectangleValue = new int[rectangleSplit.length];
 			for (int val = 0; val < rectangleSplit.length; val++) {
 				rectangleValue[val] = Integer.parseInt(rectangleSplit[val]);
-				//System.out.println(rectangleValue[val]);
 			}
 			
-			// 0 and 292 as left top pixel and make it false
-			// 399 and 307 as right bottom pixel and make it false
-			for (int pixel = 0; pixel < rectangleValue.length/2; pixel += 2){
-				grid[rectangleValue[pixel]][rectangleValue[pixel+1]] = false;
-			}
-			//System.out.println(grid[2][2]);
-		}
-		
-		
-		
-		int[] result = new int [2];
-		
-		for(int x = 0; x < grid.length; x++){
-			for(int y = 0; y < grid[x].length; y++){
-				if(grid[x][y]) {
-					// something
+			// Fill rectangle masked rectangle
+			for (int pixelX = rectangleValue[1]; pixelX <= rectangleValue[3]; pixelX++) {
+				for (int pixelY = rectangleValue[0]; pixelY <= rectangleValue[2]; pixelY++) {
+					grid[pixelX][pixelY] = false;
 				}
 			}
 		}
-
-		//System.out.println(grid[200][300]);
-		//System.out.println(result.toString());
-		return result;
-		
 	}
-	
-	public static int doFill(int x, int y){
-		
-		if(x < 0 || x >=fillX) { return 0; }
-		if (y < 0 || y >= fillY) { return 0; }
-		
-		if(!grid[x][y]) { return 0; }
-		
-		// record that you visit this node.
-		grid[x][y] = false;
-		
-		//return 1;
-		return 1 + doFill(x-1,y) + doFill(x+1,y) + doFill(x,y-1) + doFill(x,y+1);
+
+	private static int[] convertIntegersListToArray(ArrayList<Integer> result) {
+		int[] newResult = new int[result.size()];
+		Iterator<Integer> iterator = result.iterator();
+
+		for (int i = 0; i < newResult.length; i++) {
+			newResult[i] = iterator.next().intValue();
+		}
+
+		return newResult;
+	}
+
+	class Node {
+		private int x;
+		private int y;
+
+		Node(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
 	}
 }
